57cfc45fa781c935a250e259fb1579f00d14fd05
carlos-nds <carloshenriquend.santos@gmail.com>
2020-02-24 11:24:28 -0300

    Questão 1001 adcionada

---
+++ Java/1001.java
@@ -0,0 +1,19 @@
+/*
+ * Autor: Carlos Henrique
+ * Data: 24/02/2020
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);		
+		int A = keyboard.nextInt();
+		int B = keyboard.nextInt();
+		int X = A + B;
+		
+		System.out.println("X = " + X);
+	}
+}
